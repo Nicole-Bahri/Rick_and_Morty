fdc95d748e96b535b878307c7ef12c2c8978552a
Nicole Bahri <bahrinicole1@gmail.com>
2024-03-23 11:51:08 -0400

    first commit

---
+++ src/components/card.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { defineProps } from 'vue';
+    const props = defineProps({
+        character: []
+    })
+    
+</script>
+
+<template>
+    <div>
+        <div>
+          <img src="" alt="">
+        </div>
+        <div>
+           <h3>nombre</h3>
+        </div>
+    </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue';
+import card from "../components/card.vue"
+let character = ref([])
+
+async function loadCharacters(){
+  const response = await fetch(`http://rickandmortyapi.com/api/character?page=1`);
+  const data = await  response.json()
+  character.value = data.results
+  console.log(data)
+}
+
+loadCharacters()
+</script>
+
+<template>
+   <div class="flex justify-center mt-10">
+    <h1 class="text-5xl">Personajes de Rick and Morty</h1>
+   </div>
+    
+   <div v-for="personaje in character" :key="character.id">
+      {{ personaje.name }}
+   </div>
+
+   <!-- <card :character="character"/> -->
+</template>
